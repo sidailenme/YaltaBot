58ae975af5dd7ba784c63aea8fc95586cd1cb045
sidailen <sidailen.me@gmail.com>
2021-05-26 11:51:48 +0300

    create min app

---
+++ src/main/java/com/yalta/Application.java
@@ -0,0 +1,12 @@
+package com.yalta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        System.out.println("11");
+        SpringApplication.run(Application.class);
+    }
+}
